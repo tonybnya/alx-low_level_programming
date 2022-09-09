6bfb3940e96300f4d5bffef7448453498f6a96da
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-09 12:36:15 -0700

    Add: 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+			putchar(c);
+
+		c += 1;
+	}
+	printf("\n");
+
+	return (0);
+}
