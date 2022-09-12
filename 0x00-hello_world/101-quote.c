b8c25b02eb40b20ebf4bf3accc1215cfade37ef5
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-12 06:07:11 -0700

    Update: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include<unistd.h>
 
 /**
  * main - Entry point
@@ -7,6 +8,6 @@
  */
 int main(void)
 {
-	fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stderr);
-	return (0);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
