5dafed4d2b6e14a41928905ddec59971578952bc
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-13 16:42:59 -0700

    Update: 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,9 @@
 /**
  * _islower - checks for lowercase character
  *
- * Return: 1 for Success, 0 otherwise.
+ * c: the character to check
+ *
+ * Return: 1 if c is lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
