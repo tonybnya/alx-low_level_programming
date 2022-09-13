9914bb252f4cb5ce3eae455aa0ee2bfa78c5a720
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-13 13:06:49 -0700

    Update: 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - prints the alphabet in lowercase
  *
- * Return: Always 0 (Success)
+ * Return: void
  */
 void print_alphabet(void)
 {
