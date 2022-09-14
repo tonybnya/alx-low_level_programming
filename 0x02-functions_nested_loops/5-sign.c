2e3aa810e18ecae44d41b68c415839d67106f319
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-13 17:34:44 -0700

    Add: 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - print the sign of a number
+ *
+ * @n: a number
+ *
+ * Return: 1 and print + if n is greater than 0
+ * Return: 0 if n is zero
+ * Return: -1 and print - if n is less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
