bbb348b2e8c2aac0d8c90d0ea2f33cdbbf2d01c3
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-15 10:39:44 -0700

    Update: 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,6 +2,7 @@
 
 /**
  * print_numbers -  prints the numbers, from 0 to 9, followed by a new line
+ * do not print 2 and 4
  *
  * Return: void
  */
@@ -11,8 +12,10 @@ void print_most_numbers(void)
 
 	while (i <= 9)
 	{
-		if (i != 2 && i != 4)
+		if ((i != 2) && (i != 4))
+		{
 			_putchar((i % 10) + '0');
+		}
 
 		i += 1;
 	}
