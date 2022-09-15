43178641d9566eebf02148b4454625bbf406e183
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-14 17:44:14 -0700

    Add: 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+void fib(int);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int limit;
+
+	limit = 50;
+
+	fib(limit);
+
+	return (0);
+}
+
+/**
+ * fib - print the first n numbers of the fibonacci sequence
+ *
+ * Return: void
+ */
+void fib(int limit)
+{
+	long int a = 1;
+	long int b = 2;
+	long int c = a + b;
+
+	int counter = 2;
+
+	printf("%ld ", a);
+	printf("%ld ", b);
+
+	while (counter < limit)
+	{
+		printf("%ld ", c);
+		a = b;
+		b = c;
+		c = a + b;
+
+		counter += 1;
+	}
+	printf("\n");
+}
