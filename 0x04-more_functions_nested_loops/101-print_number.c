eac1d3549b4ef2659ee4e6e2166111cc108b8ac0
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-16 03:21:02 -0700

    Add: 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: integer to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar(45);
+		n = -n;
+	}
+
+	if (n / 10)
+	{
+		print_number(n / 10);
+	}
+
+	_putchar((n % 10) + '0');
+}
