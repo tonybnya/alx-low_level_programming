cec20228e0286a0a8efee1eae3af8d6d2261d769
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-19 11:43:52 -0700

    Add: 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: pointer to the character of the string
+ *
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+
+	return (i);
+}
