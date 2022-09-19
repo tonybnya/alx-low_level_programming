b63bab6ee89869a7418b4ebfaac7f915b47a159b
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-19 13:38:58 -0700

    Add: 7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *
+ * @str: the given string
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i, n, len;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		++len;
+	}
+
+	if (len % 2 == 0)
+	{
+		n = len / 2;
+	}
+	else
+	{
+		n = (len - 1) / 2;
+	}
+
+
+	for (i = n; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
