47ee244e7f1dafd81c6c1191dd911d8817d40684
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-19 12:12:51 -0700

    Add: 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts - print a string to the stdout
+ *
+ * @str - the given string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
