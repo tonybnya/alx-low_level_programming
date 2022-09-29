84ffc1548afe2ac33af8d5040d6f38fb076091ce
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-09-29 13:36:55 -0700

    Add: 6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,13 @@
 #include "main.h"
 
 /**
-   * power_operation - returns the natural square root of a number.
-    * @n: input number.
-     * @c: iterator.
-      * Return: square root or -1.
-       */
+ * power_operation - returns the natural square root of a number.
+ *
+ * @n: input number.
+ * @c: iterator.
+ *
+ * Return: square root or -1.
+ */
 int power_operation(int n, int c)
 {
 	if (c % (n / c) == 0)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * is_prime - detects if an input number is a prime number.
+ *
+ * @n: input number.
+ * @c: iterator.
+ *
+ * Return: 1 if n is a prime number. 0 if n is not a prime number.
+ */
+int is_prime(unsigned int n, unsigned int c)
+{
+	if (n % c == 0)
+	{
+		if (n == c)
+		{
+			return (1);
+		}
+		else
+		{
+			return (0);
+		}
+	}
+
+	return (0 + is_prime(n, c + 1));
+}
+
+/**
  * is_prime_number - checks if a number is prime
  *
  * @n: given number
@@ -9,17 +34,20 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n < 1)
+	if (n == 0)
 	{
 		return (0);
 	}
-	else
+
+	if (n < 0)
 	{
-		for (i = 0; i <=n; i++)
-		{
-			if (n % i == 0)
-		}
+		return (0);
 	}
+
+	if (n == 1)
+	{
+		return (0);
+	}
+
+	return (is_prime(n, 2));
 }
