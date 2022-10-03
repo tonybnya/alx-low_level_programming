33475d22d4be1de4f4f8cac7dfb4b6d222eac46b
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-03 06:01:54 -0700

    Add: 4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: 0, if digits arguments, 1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int num, digit, sum = 0;
+
+	for (num = 1; num < argc; num++)
+	{
+		for (digit = 0; argv[num][digit]; digit++)
+		{
+			if (argv[num][digit] < '0' || argv[num][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[num]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
