d5e398216334c5ea9cda2c3f1d1b84214f3ee43c
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-07 11:42:02 -0700

    Update: 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -12,17 +12,23 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	char *ptr;
 
 	if (nmemb <= 0 || size <= 0)
+	{
 		return (NULL);
+	}
 
 	ptr = malloc(nmemb * size);
 
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	nmemb *= size;
 
 	while (nmemb--)
-			ptr[nmemb] = 0;
+	{
+		ptr[nmemb] = 0;
+	}
 
 	return ((void *)ptr);
 }
