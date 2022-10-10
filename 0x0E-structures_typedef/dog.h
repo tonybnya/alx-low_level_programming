034efae58f662ba728bddbceb487a236e8b60231
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-10 03:41:32 -0700

    Update: dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,10 @@
 /**
  * struct dog - new structure
- * @name: name of the dog
- * @age: age of the dog
- * @owner: owner of the dog
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
  *
- * Description - defines a new type struct dog
+ * Description: defines a new type struct dog
  */
 struct dog
 {
