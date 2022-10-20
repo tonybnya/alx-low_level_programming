663f7da09aa6313b504be2b1c819ed98aff90c98
Tony <nya.tony2010@gmail.com>
2022-10-20 18:35:44 +0100

    Add: Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: pointer to a list_t list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+        list_t *temp;
+
+        while (head)
+        {
+                temp = head->next;
+                free(head->str);
+                free(head);
+                head = temp;
+        }
+}
