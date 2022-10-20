ee32c2b46a99f8a037f4833aa85dd3c77cbbe6f3
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-20 09:08:10 -0700

    Update: Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,31 +9,31 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-        list_t *newnode;
-        char *newstr;
-        int len = 0;
+	list_t *newnode;
+	char *newstr;
+	int len = 0;
 
-        newnode = malloc(sizeof(list_t));
-        if (newnode == NULL)
-            return (NULL);
+	newnode = malloc(sizeof(list_t));
+	if (newnode == NULL)
+	    return (NULL);
 
-        newstr = strdup(str);
-        if (newstr == NULL)
-        {
-            free(newnode);
-            return (NULL);
-        }
+	newstr = strdup(str);
+	if (newstr == NULL)
+	{
+	    free(newnode);
+	    return (NULL);
+	}
 
-        while (*str)
-        {
-            len++;
-            str++;
-        }
+	while (*str)
+	{
+	    len++;
+	    str++;
+	}
 
-        newnode->str = newstr;
-        newnode->len = len;
-        newnode->next = *head;
-        *head = newnode;
+	newnode->str = newstr;
+	newnode->len = len;
+	newnode->next = *head;
+	*head = newnode;
 
-        return (newnode);
+	return (newnode);
 }
