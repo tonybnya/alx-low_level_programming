924dff6afa0368412bb19951df71ebe32569afee
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-20 08:30:49 -0700

    Update: 1-list_len.c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,14 +8,14 @@
  */
 size_t list_len(const list_t *h)
 {
-        size_t count;
+	size_t count;
 
-        count = 0;
-        while (h)
-        {
-                count++;
-                h = h->next;
-        }
+	count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
 
-        return (count);
+	return (count);
 }
