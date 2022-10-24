45537068beeb3b49836a212cbf45d78f03322a08
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-24 11:38:19 -0700

    Update: Task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_nodeint - adds a new node at the end of a listint_t list
+ * add_nodeint_end - adds a new node at the end of a listint_t list
  * @head: pointer to the address of the first node
  * @n: integer as content of the new node
  *
