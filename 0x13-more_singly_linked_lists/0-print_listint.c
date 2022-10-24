75e46e6ceb9206a1850fbcf285a0eb3952bee082
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-10-24 10:38:08 -0700

    Update: Task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,11 +11,11 @@ size_t print_listint(const listint_t *h)
 	size_t nodes;
 
 	nodes = 0;
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
 	}
 
 	return (nodes);
