67bb239c617349c2025d018731c49276a616a13c
Tony <nya.tony2010@gmail.com>
2022-12-09 17:20:45 +0100

    Update: 0-print_dlistint.c

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -7,4 +7,18 @@
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
-{}
+{
+        size_t nodes;
+        const dlistint_t *temp;
+
+        nodes = 0;
+        temp = h;
+        while (temp != NULL)
+        {
+                printf("%d\n", temp->n);
+                temp = temp->next;
+                nodes++;
+        }
+
+        return (nodes);
+}
