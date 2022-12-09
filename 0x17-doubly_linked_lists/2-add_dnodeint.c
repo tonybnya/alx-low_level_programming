b234fd525371bfd961a75f888be166d508d160f9
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-09 14:45:27 -0800

    Update: 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_dnodeint - adds a new node at the beginning of a dlistint_t list.
+ * add_dnodeint - adds a new node at the beginning of a dlistint_t list
  * @head: points to the front of the doubly linked list
  * @n: the integer
  *
@@ -9,29 +9,29 @@
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-        dlistint_t *node;
-        dlistint_t *h;
-
-        node = malloc(sizeof(dlistint_t));
-        if (node == NULL)
-                return (NULL);
-
-        node->n = n;
-        node->prev = NULL;
-        h = *head;
-
-        if (h != NULL)
-        {
-                while (h->prev != NULL)
-                        h = h->prev;
-        }
-
-        node->next = h;
-
-        if (h != NULL)
-                h->prev = node;
-
-        *head = node;
-
-        return (node);
+	dlistint_t *node;
+	dlistint_t *h;
+	
+	node = malloc(sizeof(dlistint_t));
+	if (node == NULL)
+		return (NULL);
+	
+	node->n = n;
+	node->prev = NULL;
+	h = *head;
+	
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+	
+	node->next = h;
+	
+	if (h != NULL)
+		h->prev = node;
+	
+	*head = node;
+	
+	return (node);
 }
