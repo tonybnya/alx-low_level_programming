ba55ea5898edc5533723cd0d7332ef1a863639fd
Tony Blondeau NYA <nya.tony2010@gmail.com>
2022-12-09 06:01:06 -0800

    Add: '0x17-doubly_linked_lists' directory

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ *
+ * @s: the given string
+ *
+ * Return: the corresponding integer
+ */
+int _atoi(char *s)
+{
+	int i;
+	int num;
+	int sign = 1;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if
+	}
+}
