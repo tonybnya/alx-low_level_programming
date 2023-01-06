3c4662986250874dcb9224505ac65ed4d9c645b8
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-06 11:26:55 -0800

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,23 +8,23 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-        hash_table_t *ht;
-        hash_node_t **array;
-        unsigned long int i;
+	hash_table_t *ht;
+	hash_node_t **array;
+	unsigned long int i;
 
-        ht = malloc(sizeof(hash_table_t));
-        if (ht == NULL)
-                return (NULL);
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
+		return (NULL);
 
-        array = malloc(sizeof(hash_node_t *) * size);
-        if (array == NULL)
-                return (NULL);
+	array = malloc(sizeof(hash_node_t *) * size);
+	if (array == NULL)
+		return (NULL);
 
-        for (i = 0; i < size; i++)
-                array[i] = NULL;
+	for (i = 0; i < size; i++)
+		array[i] = NULL;
 
-        ht->size = size;
-        ht->array = array;
+	ht->size = size;
+	ht->array = array;
 
-        return (ht);
+	return (ht);
 }
