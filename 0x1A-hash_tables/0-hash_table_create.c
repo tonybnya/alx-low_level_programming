c632d538e74c9957df27d4ffcd4d7dae49c97ee5
Tony <nya.tony2010@gmail.com>
2023-01-10 08:38:00 +0100

    update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -21,7 +21,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	for (i = 0; i < size; i++)
-		ht->array[i] = NULL;
+		array[i] = NULL;
 
 	ht->size = size;
 	ht->array = array;
