c720868eb0b1076ab654a7bb08db52f9998ce997
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-10 03:50:17 -0800

    update

--- 0x1A-hash_tables/tests/1-main.c
+++ 0x1A-hash_tables/tests/1-main.c
@@ -1,4 +1,4 @@
-#include "hash_tables.h"
+#include "../hash_tables.h"
 
 /**
  * main - check the code
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-        char *s;
+	char *s;
 
-        s = "cisfun";
-        printf("%lu\n", hash_djb2((unsigned char *)s));
+	s = "cisfun";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
 
-        s = "Don't forget to tweet today";
-        printf("%lu\n", hash_djb2((unsigned char *)s));
+	s = "Don't forget to tweet today";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
 
-        s = "98";
-        printf("%lu\n", hash_djb2((unsigned char *)s));
+	s = "98";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
 
-        return (EXIT_SUCCESS);
+	return (EXIT_SUCCESS);
 }
