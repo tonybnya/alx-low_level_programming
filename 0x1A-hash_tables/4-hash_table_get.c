0e4170ed37b24a590ec0f56e097ab78d514774a5
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-11 11:01:25 -0800

    update

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,22 +10,22 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-        unsigned long int index;
-        hash_node_t *temp;
+	unsigned long int index;
+	hash_node_t *temp;
 
-        if (ht == NULL || key == NULL || *key == '\0')
-                return (NULL);
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
 
-        index = key_index((unsigned char *)key, ht->size);
-        temp = ht->array[index];
+	index = key_index((unsigned char *)key, ht->size);
+	temp = ht->array[index];
 
-        while (temp != NULL)
-        {
-                if (strcmp(temp->key, key) == 0)
-                        return (temp->value);
+	while (temp != NULL)
+	{
+		if (strcmp(temp->key, key) == 0)
+			return (temp->value);
 
-                temp = temp->next;
-        }
+		temp = temp->next;
+	}
 
-        return (NULL);
+	return (NULL);
 }
