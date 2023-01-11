d28061e60ef1a27f63557d82fe3f735d23bc02b4
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-01-11 11:17:13 -0800

    update

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,26 +6,26 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-        hash_table_t *head = ht;
-        hash_node_t *node, *temp;
-        unsigned long int i;
+	hash_table_t *head = ht;
+	hash_node_t *node, *temp;
+	unsigned long int i;
 
-        for (i = 0; i < ht->size; i++)
-        {
-                if (ht->array[i] != NULL)
-                {
-                        node = ht->array[i];
-                        while (node != NULL)
-                        {
-                                temp = node->next;
-                                free(node->key);
-                                free(node->value);
-                                free(node);
-                                node = temp;
-                        }
-                }
-        }
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i] != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				temp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = temp;
+			}
+		}
+	}
 
-        free(head->array);
-        free(head);
+	free(head->array);
+	free(head);
 }
