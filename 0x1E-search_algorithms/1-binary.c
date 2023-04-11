abd55169f5c9016deac1e620b33d0ddff41bee1d
Tony <nya.tony2010@gmail.com>
2023-04-12 00:34:41 +0100

    Add: 3-0 & 1-binary.c

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - Binary Search Algorithm on an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in the array
+ * @value: value to search for
+ *
+ * Return: the index where value is located
+ * -1 if value is not present in the array or if array is NULL
+ */
+int binary_search(int *array, size_t size, int value)
+{
+        size_t left = 0;
+}
