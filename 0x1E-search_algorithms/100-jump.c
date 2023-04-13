74003ca88630e5d8225ade48deb47555feea3b73
Tony Blondeau NYA <nya.tony2010@gmail.com>
2023-04-13 11:59:23 -0700

    Update: 100-jump.c

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -12,7 +12,7 @@
 int jump_search(int *array, size_t size, int value)
 {
 	int step = sqrt(size);
-        int prev = 0;
+	int prev = 0;
 	char str1[] = "Value checked array";
 	char str2[] = "Value found between indexes";
 
@@ -23,28 +23,28 @@ int jump_search(int *array, size_t size, int value)
 
 	while (array[(int)(fmin(step, size) - 1)] < value)
 	{
-                prev = step;
-                step += sqrt(size);
+		prev = step;
+		step += sqrt(size);
 
-                if (prev >= (int)size)
-                        return (-1);
+		if (prev >= (int)size)
+			return (-1);
 
-                printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
+		printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
 	}
 
-        while (array[prev] < value)
-        {
-                prev++;
-                if (prev == fmin(step, size))
-                        return (-1);
-                printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
-        }
+	while (array[prev] < value)
+	{
+		prev++;
+		if (prev == fmin(step, size))
+			return (-1);
+		printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
+	}
 
-        if (array[prev] == value)
-        {
-                printf("%s [%d] and [%d]\n", str2, prev, prev);
-                return (prev);
-        }
+	if (array[prev] == value)
+	{
+		printf("%s [%d] and [%d]\n", str2, prev, prev);
+		return (prev);
+	}
 
 	return (-1);
 }
