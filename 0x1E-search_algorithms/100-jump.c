e88399cf9a666197989191c76d29cb96b48ab9a0
tonybnya <nya.tony2010@gmail.com>
2023-05-04 06:08:31 -0700

    betty update on 100-jump.c file

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -11,7 +11,7 @@
  */
 int jump_search(int *array, size_t size, int value)
 {
-        int first_occ = 0;
+	int first_occ = 0;
 	int prev = 0;
 	int step = sqrt(size);
 	char str1[] = "Value checked array";
@@ -19,19 +19,15 @@ int jump_search(int *array, size_t size, int value)
 
 	if (array == NULL)
 		return (-1);
-
 	while (array[(int)(fmin(step, size) - 1)] < value)
 	{
 		printf("%s[0] = [%d]\n", str1, array[0]);
 		prev = step;
 		step += sqrt(size);
-
 		if (prev >= (int)size)
 			return (-1);
-
 		printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
 	}
-
 	while (array[prev] < value)
 	{
 		prev++;
@@ -39,24 +35,15 @@ int jump_search(int *array, size_t size, int value)
 			return (-1);
 		printf("%s[%d] = [%d]\n", str1, prev, array[prev]);
 	}
-
-        while (array[prev] == value && prev < fmin(step, size))
-        {
-                printf("%s[%d] = [%d]\n", str2, prev, array[prev]);
-                prev++;
-        }
-
-        if (prev == fmin(step, size) && array[prev - 1] == value)
-        {
-                printf("%s [%d] and [%d]\n", str2, first_occ, prev - 1);
-                return (first_occ);
-        }
-
-	/* if (array[prev] == value) */
-	/* { */
-	/* 	printf("%s [%d] and [%d]\n", str2, prev, prev); */
-	/* 	return (prev); */
-	/* } */
-
+	while (array[prev] == value && prev < fmin(step, size))
+	{
+		printf("%s[%d] = [%d]\n", str2, prev, array[prev]);
+		prev++;
+	}
+	if (prev == fmin(step, size) && array[prev - 1] == value)
+	{
+		printf("%s [%d] and [%d]\n", str2, first_occ, prev - 1);
+		return (first_occ);
+	}
 	return (-1);
 }
